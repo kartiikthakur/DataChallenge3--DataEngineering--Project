665285205ce3bad7472cfc3e13330da54fd707d5
kartik thakur <kartiikthakur@gmail.com>
2020-03-27 20:12:05 -0400

    final

--- src/complaints.py
+++ src/complaints.py
@@ -1,3 +1,9 @@
+"""
+Author: Kartik Singh Thakur
+Date: 3/27/2020
+Insight Data Engineering Challenge
+"""
+
 import os
 import csv
 
@@ -8,12 +14,11 @@ def listToString(s):
         str1 += ele
     return str1
 
+
+# Get Input file location
 def input():
-    # input_folder = os.path.abspath('../input')
-    # input_folder2 = os.path.abspath('../consumer_complaints/input')
-    input_folder = os.path.abspath('../insight_testsuite/tests/test_1/input')
-    input_folder2 = os.path.abspath('../insight_testsuite/tests/test_1/input')
-    print(input_folder)
+    input_folder = os.path.abspath('../input')
+    input_folder2 = os.path.abspath('../consumer_complaints/input')
     try:
         filename, file_extension = os.path.splitext(os.listdir(input_folder)[0])
         return input_folder, filename, file_extension
@@ -23,6 +28,7 @@ def input():
         return input_folder2, filename, file_extension
 
 
+# Extracting Data from Input file location
 def extraction(input_file):
     output_list = []
     unique_product_year = []
@@ -40,16 +46,20 @@ def extraction(input_file):
                     continue
 
                 product = row[1]
-                if "," in product:
+                if "," in product:  # Add double quotes if a "," exists in the Product name
                     new_product = "\"" + product + "\""
                     row[1] = listToString(new_product)
 
+                # Creating a dictionary for all the rows in the input file
                 each_row = {}
                 for key, Column in enumerate(columns):
                     each_row[Column] = row[key]
 
+                # Extracting all combinations of Product and Year match
                 each_row['ProductYear'] = (row[1].lower() + "," + row[0].split("-")[0])
                 all_product_year.append(each_row['ProductYear'])
+
+                # Extracting Unique combinations of Product and Year match
                 if each_row['ProductYear'] not in unique_product_year:
                     unique_product_year.append(each_row['ProductYear'])
 
@@ -64,9 +74,12 @@ def extraction(input_file):
     return output_list, unique_product_year, all_product_year
 
 
+# Transforming Data as per the requirements in the problem statement
 def transform(data, unique_lists, all_lists):
-    unique_lists.sort(key=lambda x: x[1:len(x) - 1] if x[0] == '"' else x[0:len(x) - 1])
+    unique_lists.sort(key=lambda x: x[1:len(x) - 1] if x[0] == '"' else x[0:len(x) - 1])  # Sorting the Products
     rows = []
+
+    # Calculate Count, Sum, Percentage of Unique Product and Year match
     for unique_list in unique_lists:
         all_companies = [x['Company'] for x in data if x['ProductYear'] == unique_list]
         unique_company = set(all_companies)
@@ -82,17 +95,17 @@ def transform(data, unique_lists, all_lists):
     return rows
 
 
+# Writing the final data in a csv file
 def load(rows):
-    # output_folder = os.path.abspath('../output')
-    # output_folder2 = os.path.abspath('../consumer_complaints/output')
-    output_folder = os.path.abspath('../insight_testsuite/tests/test_1/output')
-    output_folder2 = os.path.abspath('../insight_testsuite/tests/test_1/output')
+    output_folder = os.path.abspath('../output')
+    output_folder2 = os.path.abspath('../consumer_complaints/output')
     try:
         output_file = output_folder + '/' + 'report.csv'
     except:
         output_file = output_folder2 + '/' + 'report.csv'
 
     print(output_file)
+    # Writing the final data into the report.csv file
     with open(output_file, 'w') as f:
         for x in rows:
             f.write(','.join(x) + '\n')
@@ -101,9 +114,8 @@ def load(rows):
 
 
 if __name__ == "__main__":
-        input_folder, filename, file_extension = input()
-        input_csv = os.path.join(input_folder,filename+file_extension)
-        extracted_data, unique_ProductYear, all_ProductYear = extraction(input_csv)
-        transformed_data = transform(extracted_data, unique_ProductYear, all_ProductYear)
-        load(transformed_data)
-
+    input_folder, filename, file_extension = input()
+    input_csv = os.path.join(input_folder, filename + file_extension)
+    extracted_data, unique_ProductYear, all_ProductYear = extraction(input_csv)
+    transformed_data = transform(extracted_data, unique_ProductYear, all_ProductYear)
+    load(transformed_data)
